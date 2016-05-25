9539b65f02c4bd964b885654e8c7eb884a44bf91
Egor Zhelobanov <e.zhelobanov@gmail.com>
2016-05-26 10:57:45 +1100

    Added indentations

--- src/config/static-pages.php
+++ src/config/static-pages.php
@@ -2,10 +2,10 @@
 
 return [
     'static-pages' => [
-    'name' => 'static-pages',
-    'url' => 'static-pages::admin::index',
-    'caption' => 'Static Pages',
-    'icon' => 'fa fa-file-text-o',
-    'active' => 'static-pages::admin::index',
-    ]
+        'name' => 'static-pages',
+        'url' => 'static-pages::admin::index',
+        'caption' => 'Static Pages',
+        'icon' => 'fa fa-file-text-o',
+        'active' => 'static-pages::admin::index',
+    ],
 ];
