c624948ac4d1731b69f3378329c51f70c5038085
Egor Zhelobanov <e.zhelobanov@gmail.com>
2016-06-10 17:42:09 +1100

    Created view of page

--- src/Http/Controllers/StaticPagesUserController.php
+++ src/Http/Controllers/StaticPagesUserController.php
@@ -12,14 +12,17 @@ class StaticPagesUserController extends Controller
     {
         $pages = $staticPages->where('status', $staticPages::STATUS_ACTIVE)->orderBy('updated_at', 'desc')->get();
 
-        \Title::prepend(trans('dashboard.title.prepend'));
-        \Title::append(trans('static-pages::pages.index.title'));
-
         return view('static-pages::site.pages-list', compact('pages'));
     }
 
-    public function pageView()
+    public function pageView(StaticPage $staticPages, $slug)
     {
-        return view('static-pages::site.pages-view');
+        $page = $staticPages->where('status', $staticPages::STATUS_ACTIVE)->where('slug', $slug)->get()->first();
+
+        if (is_null($page)) {
+            return abort('404');
+        }
+
+        return view('static-pages::site.pages-view', compact('page'));
     }
 }
