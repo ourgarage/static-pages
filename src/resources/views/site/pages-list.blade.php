54208848f206974f6555243d716cd2c7a5cf7c2b
Egor Zhelobanov <egor@zhelobanov.ru>
2016-06-11 22:02:46 +1100

    Fixed users views

--- src/resources/views/site/pages-list.blade.php
+++ src/resources/views/site/pages-list.blade.php
@@ -1,17 +1,11 @@
 @extends('site.main')
 
 @section('body')
-
     <div class="container">
-
         <div class="row">
-
             <div class="col-md-8 col-md-offset-2">
-
                 <div class="panel panel-default">
-
                     <div class="panel-heading">{{ trans('static-pages::pages.view.header') }}</div>
-
                     <div class="panel-body">
                         @if(!$pages->isEmpty())
                             <ul>
@@ -27,13 +21,8 @@
                             {{ trans('static-pages::pages.view.no-pages') }}
                         @endif
                     </div>
-
                 </div>
-
             </div>
-
         </div>
-
     </div>
-
 @stop
