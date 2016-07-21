8dd41cfce402ef851d8207c6084d1097f36c33c4
Egor Zhelobanov <e.zhelobanov@gmail.com>
2016-07-21 12:47:53 +1100

    Changed format of config file

--- src/config/static-pages.php
+++ src/config/static-pages.php
@@ -1,19 +1,23 @@
 <?php
 
 return [
-    'static-pages' => [
-        'name' => 'static-pages',
-        'url' => 'static-pages::admin::index',
-        'caption' => 'Static Pages',
-        'icon' => 'fa fa-file-text-o',
-        'active' => 'static-pages::admin::index',
 
-        'settings' => [
-            'url' => '#',
+    'static-pages' => [
+
+        'name' => 'static-pages',
+
+        'menu' => [
+            'url' => 'static-pages::admin::index',
+            'caption' => 'Static Pages',
+            'icon' => 'fa fa-file-text-o',
+            'active' => 'static-pages::admin::index',
+        ],
+
+        'menu-settings' => [
+            'url' => 'static-pages::admin::index',
             'caption' => 'Static pages settings',
             'icon' => 'fa fa-cog',
-            'active' => '#',
+            'active' => 'static-pages::admin::index',
         ],
     ],
-
 ];
